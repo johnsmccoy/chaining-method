2fda222eca5c06f6a86008e0e4e6d9cb343604b5
johnsmccoy <johnsmccoy@comcast.net>
2018-04-27 13:02:59 -0500

    added another way to complete the problem

--- chaining.js
+++ chaining.js
@@ -6,10 +6,15 @@
 
 const integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
 
-const finalIntegers = integers
-.sort((a,b) => b - a)
-.filter(currentInteger => currentInteger <= 19)
-.map(i =>(i * 1.5) - 1)
-.reduce((first, second) => first + second)
+// const finalIntegers = integers
+// .sort((a,b) => b - a)
+// .filter(currentInteger => currentInteger <= 19)
+// .map(i =>(i * 1.5) - 1)
+// .reduce((first, second) => first + second)
 
 
+const integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8]
+
+const chained = integers.sort((a,b) => a-b).reverse().filter(int =>{return((int <= 19))}).map(function (int){return ((int * 1.5) -1)})
+
+console.log(chained.reduce(function (total, current){return total += current}, 0))
